996b5d6d9a951cdd9f650cee97667a8abe2fd004
Ben Greenman <benjaminlgreenman@gmail.com>
2017-08-15 13:53:56 -0400

    benchmarks: data from 12 optimization branches

--- benchmarks/rkt/run-all.sh
+++ benchmarks/rkt/run-all.sh
@@ -1,5 +1,6 @@
 ITERS=20
-for BM in acquire-worst array combinations dungeon-worst forth-bad forth-worst fsm-bad fsmoo-bad fsmoo-worst fsm-worst-case graph gregor-worst hanoi jpeg kcfa-worst lnm-worst mbta-worst morsecode-worst quadBG-worst quadMB-bad quadMB-worst snake-worst suffixtree-worst synth synth-worst take5-worst tetris-worst trie trie-vector zombie-worst zordoz-worst; do
+# fsmoo-bad fsmoo-worst kcfa-worst
+for BM in acquire-worst array combinations dungeon-worst forth-bad forth-worst fsm-bad fsm-worst-case graph gregor-worst hanoi jpeg lnm-worst mbta-worst morsecode-worst quadBG-worst quadMB-bad quadMB-worst snake-worst suffixtree-worst synth synth-worst take5-worst tetris-worst trie trie-vector zombie-worst zordoz-worst; do
   cd ${BM};
   for R in "master" "base" "mono" "poly" "spec" "anyc" "spec-anyc" "mono-anyc" "poly-anyc" "mono-spec" "poly-spec" "mono-spec-anyc" "poly-spec-anyc"; do
     RCO="/home/ben/code/racket/racket-contract/${R}/racket/bin/raco"
