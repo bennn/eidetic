75f7a891e0c35be45ed65ee2406c7ae10b179057
Ben Greenman <benjaminlgreenman@gmail.com>
2017-08-17 20:20:28 -0400

    benchmarks: comparing SE 10

--- benchmarks/rkt/run-all.sh
+++ benchmarks/rkt/run-all.sh
@@ -1,11 +1,12 @@
 ITERS=20
-# fsmoo-bad fsmoo-worst kcfa-worst
-for BM in acquire-worst array combinations dungeon-worst forth-bad forth-worst fsm-bad fsm-worst-case graph gregor-worst hanoi jpeg lnm-worst mbta-worst morsecode-worst quadBG-worst quadMB-bad quadMB-worst snake-worst suffixtree-worst synth synth-worst take5-worst tetris-worst trie trie-vector zombie-worst zordoz-worst; do
-  cd ${BM};
-  for R in "master" "base" "mono" "poly" "spec" "anyc" "spec-anyc" "mono-anyc" "poly-anyc" "mono-spec" "poly-spec" "mono-spec-anyc" "poly-spec-anyc"; do
+#for BM in acquire-worst array combinations dungeon-worst forth-bad forth-worst fsm-bad fsm-worst-case graph gregor-worst hanoi jpeg lnm-worst mbta-worst morsecode-worst quadBG-worst quadMB-bad quadMB-worst snake-worst suffixtree-worst synth synth-worst take5-worst tetris-worst trie trie-vector zombie-worst zordoz-worst; do
+for BM in combinations trie-vector jpeg synth-worst suffixtree-worst acquire-worst array dungeon-worst forth-bad forth-worst fsm-bad fsm-worst-case graph gregor-worst hanoi lnm-worst mbta-worst morsecode-worst quadBG-worst quadMB-bad quadMB-worst snake-worst take5-worst tetris-worst trie zombie-worst zordoz-worst; do
+  cd ${BM} && \
+  for R in "base" "base-no-gunk" "base-enter-s-e-on-10"; do
     RCO="/home/ben/code/racket/racket-contract/${R}/racket/bin/raco"
     RKT="/home/ben/code/racket/racket-contract/${R}/racket/bin/racket"
     find . -name "compiled" | xargs rm -r; ${RCO} make -v main.rkt; for i in `seq 0 ${ITERS}`; do ${RKT} main.rkt >> ${R}.txt; done;
+    mv ${R}.txt ../output-2017-08-20/${BM}-${R}.txt
   done
   cd ..;
 done;
