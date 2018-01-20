ce481a0700762980ad415965240de6ebd2065acb
Ben Greenman <benjaminlgreenman@gmail.com>
2018-01-20 15:05:23 -0500

    benchmark: increase 'graph' running time
    Run the same tests on a second matrix, twice as large as the first.
    
    The new matrix is just the first doubled. It's not designed at all.

--- benchmarks/rkt/graph/main.rkt
+++ benchmarks/rkt/graph/main.rkt
@@ -13,10 +13,48 @@
                     [2 #f -5 0 #f]
                     [#f #f #f 6 0]]))
 
+(define matrix25.2
+  (mk-matrix-graph [[0 3 8 #f -4 0 3 8 #f -4]
+                    [#f 0 #f 1 7 #f 0 #f 1 7]
+                    [#f 4 0 #f #f #f 4 0 #f #f]
+                    [2 #f -5 0 #f 2 #f -5 0 #f]
+                    [#f #f #f 6 0 #f #f #f 6 0]
+                    [0 3 8 #f -4 0 3 8 #f -4]
+                    [#f 0 #f 1 7 #f 0 #f 1 7]
+                    [#f 4 0 #f #f #f 4 0 #f #f]
+                    [2 #f -5 0 #f 2 #f -5 0 #f]
+                    [#f #f #f 6 0 #f #f #f 6 0]]))
+
+#;(define matrix25.4
+  (mk-matrix-graph [[0 3 8 #f -4 0 3 8 #f -4 0 3 8 #f -4 0 3 8 #f -4]
+                    [#f 0 #f 1 7 #f 0 #f 1 7 #f 0 #f 1 7 #f 0 #f 1 7]
+                    [#f 4 0 #f #f #f 4 0 #f #f #f 4 0 #f #f #f 4 0 #f #f]
+                    [2 #f -5 0 #f 2 #f -5 0 #f 2 #f -5 0 #f 2 #f -5 0 #f]
+                    [#f #f #f 6 0 #f #f #f 6 0 #f #f #f 6 0 #f #f #f 6 0]
+                    [0 3 8 #f -4 0 3 8 #f -4 0 3 8 #f -4 0 3 8 #f -4]
+                    [#f 0 #f 1 7 #f 0 #f 1 7 #f 0 #f 1 7 #f 0 #f 1 7]
+                    [#f 4 0 #f #f #f 4 0 #f #f #f 4 0 #f #f #f 4 0 #f #f]
+                    [2 #f -5 0 #f 2 #f -5 0 #f 2 #f -5 0 #f 2 #f -5 0 #f]
+                    [#f #f #f 6 0 #f #f #f 6 0 #f #f #f 6 0 #f #f #f 6 0]
+                    [0 3 8 #f -4 0 3 8 #f -4 0 3 8 #f -4 0 3 8 #f -4]
+                    [#f 0 #f 1 7 #f 0 #f 1 7 #f 0 #f 1 7 #f 0 #f 1 7]
+                    [#f 4 0 #f #f #f 4 0 #f #f #f 4 0 #f #f #f 4 0 #f #f]
+                    [2 #f -5 0 #f 2 #f -5 0 #f 2 #f -5 0 #f 2 #f -5 0 #f]
+                    [#f #f #f 6 0 #f #f #f 6 0 #f #f #f 6 0 #f #f #f 6 0]
+                    [0 3 8 #f -4 0 3 8 #f -4 0 3 8 #f -4 0 3 8 #f -4]
+                    [#f 0 #f 1 7 #f 0 #f 1 7 #f 0 #f 1 7 #f 0 #f 1 7]
+                    [#f 4 0 #f #f #f 4 0 #f #f #f 4 0 #f #f #f 4 0 #f #f]
+                    [2 #f -5 0 #f 2 #f -5 0 #f 2 #f -5 0 #f 2 #f -5 0 #f]
+                    [#f #f #f 6 0 #f #f #f 6 0 #f #f #f 6 0 #f #f #f 6 0] ]))
+
 (define (main)
-  (define matrix25.1res/slow (all-pairs-shortest-paths/slow matrix25.1))
-  (define matrix25.1res/faster (all-pairs-shortest-paths/faster matrix25.1))
-  (define matrix25.1res/floyd (floyd-warshall matrix25.1))
+  (void
+    (all-pairs-shortest-paths/slow matrix25.1)
+    (all-pairs-shortest-paths/faster matrix25.1)
+    (floyd-warshall matrix25.1)
+    (all-pairs-shortest-paths/slow matrix25.2)
+    (all-pairs-shortest-paths/faster matrix25.2)
+    (floyd-warshall matrix25.2))
   (void))
 
 (time (main))
