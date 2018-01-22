f294f77c6e27975388f898b3b7e1d4831893b843
Ben Greenman <benjaminlgreenman@gmail.com>
2018-01-21 22:35:28 -0500

    benchmark: add plot3d use to plot benchmark
    Increases running time by 1-2 seconds,
     does not seem to enter space-efficient mode

--- benchmarks/rkt/plot/main.rkt
+++ benchmarks/rkt/plot/main.rkt
@@ -1,9 +1,10 @@
 #lang racket/base
-(require plot/compat)
+(require plot/no-gui (only-in plot/compat gradient))
 
 (define (trig x y) (* (sin x) (sin y)))
 
 (time (void
-  (plot (mix (shade trig) (contour trig) (vector-field (gradient trig) #:samples 25 #:color 'black))
+  (plot3d-pict (isosurfaces3d (compose abs max) -1 1 -1 1 -1 1))
+  (plot-pict (mix (shade trig) (contour trig) (vector-field (gradient trig) #:samples 25 #:color 'black))
     #:x-min -1.5 #:x-max 1.5 #:y-min -1.5 #:y-max 1.5
     #:title "gradient field +shdade + contours of F(x,y) = sin(x) * sin(y)")))
