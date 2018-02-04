73cc3135ea623407a8a0b62fcb1c0d9d6a5f2672
Ben Greenman <benjaminlgreenman@gmail.com>
2018-02-03 19:49:27 -0500

    benchmark: output for some chaperones case studies

--- benchmarks/rkt/run-all.sh
+++ benchmarks/rkt/run-all.sh
@@ -1,7 +1,8 @@
-ITERS=20
+ITERS=6
 #  fsmoo-bad fsmoo-worst
-OUTPUT="output/2018-01-26"
-for BM in acquire-worst array combinations dungeon-worst forth-bad forth-worst fsm-bad fsm-worst-case graph gregor-worst hanoi jpeg kcfa-worst lnm-worst mbta-worst morsecode-worst quadBG-worst quadMB-bad quadMB-worst snake-worst suffixtree-worst synth synth-worst take5-worst tetris-worst trie trie-vector zombie-worst zordoz-worst; do
+OUTPUT="output/2018-02-02"
+#for BM in acquire-worst array combinations dungeon-worst forth-bad forth-worst fsm-bad fsm-worst-case graph gregor-worst hanoi jpeg kcfa-worst lnm-worst mbta-worst morsecode-worst quadBG-worst quadMB-bad quadMB-worst snake-worst suffixtree-worst synth synth-worst take5-worst tetris-worst trie trie-vector zombie-worst zordoz-worst; do
+for BM in bubblesort determinance guide-render science slideshow; do 
   cd ${BM} && \
   for R in "6.12"; do
     # run a Racket release
@@ -10,12 +11,12 @@ for BM in acquire-worst array combinations dungeon-worst forth-bad forth-worst f
     find . -name "compiled" | xargs rm -r; ${RCO} make -v main.rkt; for i in `seq 0 ${ITERS}`; do ${RKT} main.rkt >> ${R}.txt; done;
     mv ${R}.txt ../${OUTPUT}/${BM}-${R}.txt
   done
-  #for R in "base" "base-no-gunk" "base-enter-s-e-on-10"; do
-  #  # run space-efficient Rackets
-  #  RCO="/home/ben/code/racket/racket-contract/${R}/racket/bin/raco"
-  #  RKT="/home/ben/code/racket/racket-contract/${R}/racket/bin/racket"
-  #  find . -name "compiled" | xargs rm -r; ${RCO} make -v main.rkt; for i in `seq 0 ${ITERS}`; do ${RKT} main.rkt >> ${R}.txt; done;
-  #  mv ${R}.txt ../${OUTPUT}/${BM}-${R}.txt
-  #done
+  for R in "master"; do
+    # run space-efficient Rackets
+    RCO="/home/ben/code/racket/racket-contract/${R}/racket/bin/raco"
+    RKT="/home/ben/code/racket/racket-contract/${R}/racket/bin/racket"
+    find . -name "compiled" | xargs rm -r; ${RCO} make -v main.rkt; for i in `seq 0 ${ITERS}`; do ${RKT} main.rkt >> ${R}.txt; done;
+    mv ${R}.txt ../${OUTPUT}/${BM}-${R}.txt
+  done
   cd ..;
 done;
